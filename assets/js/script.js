ca9b36877177e73fc6c4acd7b6a9beb434b141fa
Adam Grise <adamwgrise@gmail.com>
2019-05-28 07:37:31 -0500

    Buncha updates to js; player1 is working!

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,31 +11,93 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
-console.log(database.ref.rpsGame);
 
-/////////////// HOW TO PUT STUFF ON THE DB ///////////////
-var counter = 0;
+// GLOBALS
+var p1choice = '';
+
+/////////////// FUNCTIONS ON PAGE LOAD /////////////////
+
+var loadDisplay = function () {
+    database.ref().once('value', function (snapshot) {
+        console.log(snapshot.val());
+        console.log(snapshot.val().p1exists);
+        if (snapshot.val().p1exists === true) {
+            $('#p1-userInput').hide();
+            $('#p1-exit').show();
+            $('.p1choice').show();
+            $('#p1-display').show();
+            console.log('on loadDisplay, snapshot.player1 is true');
+        } else {
+            $('#p1-userInput').show();
+            $('#p1-exit').hide();
+            $('.p1choice').hide();
+            $('#p1-display').hide();
+            console.log('on loadDisplay, snapshot.player1 is false');
+        }
+    });
+};
+
+/////////////// CLICK EVENTS ///////////////
+
+// New player submission
 $('#p1-submitButton').click(function (e) {
     e.preventDefault();
-    counter++;
-
     database.ref().set({
-        clickCount: counter
+        p1exists: true,
+        player1: {
+            p1name: $('#p1-name').val().trim(),
+            p1wins: 0,
+            p1loss: 0,
+            p1chat: ''
+        }
     });
-
+    $('#p1-userInput').hide();
+    $('#p1-exit').show();
+    $('.p1choice').show();
+    $('#p1-display').show();
 });
 
-////////////// HOW TO GET STUFF FROM DB /////////////////
-database.ref().on("value", function (snapshot) {
-    // Then we console.log the value of snapshot
+$('#p1-exit').click(function (e) {
+    e.preventDefault();
+    database.ref().set({
+        p1exists: false
+    });
+    removeRef = database.ref('player1');
+    removeRef.remove()
+        .then(function () {
+            console.log('Player 1 removed.');
+            $('#p1-userInput').show();
+            $('#p1-exit').hide();
+            $('.p1choice').hide();
+            $('#p1-display').hide();
+        });
+});
+
+$('.p1choice').click(function () {
+    p1choice = $(this).attr('value');
+    console.log('p1choice: ' + p1choice);
+    p1selected = true;
+});
+
+////////////// PULLS ON DATA CHANGES /////////////////
+
+// Event listener for any changes
+database.ref().on('value', function (snapshot) {
+    console.log('snapshot.val');
     console.log(snapshot.val());
-    // Update the clickCounter variable with data from the database.
-    counter = snapshot.val().clickCount;
-    // Then we change the html associated with the number.
-    $("#display").text(snapshot.val().clickCount);
-    // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-    // Again we could have named errorObject anything we wanted.
+    if (snapshot.val().p1exists === true) {
+        console.log('on value update, snapshot.player1 is true');
+        p1name = snapshot.val().player1.p1name; // Updating local var
+        $('#p1-display').text('Player 1: ' + p1name); // Displaying local var
+    };
 }, function (errorObject) {
-    // In case of error this will print the error
-    console.log("The read failed: " + errorObject.code);
+    console.log('The read failed: ' + errorObject.code);
 });
+
+loadDisplay();
+
+/*
+Hide everything but player data on load, as well as exit game buttons
+show rps upon submitting name
+hide name submission option if there's already a player
+*/
