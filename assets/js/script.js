178a0b4ba4edd94be177deb253e242cd751adc6b
Adam Grise <adamwgrise@gmail.com>
2019-05-28 23:21:47 -0500

    Player2 stuff in - trying to figure out firebase data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,9 @@ var database = firebase.database();
 
 // GLOBALS
 var p1choice = '';
+var p2choice = '';
+var p1selected = false;
+var p2selected = false;
 
 /////////////// FUNCTIONS ON PAGE LOAD /////////////////
 
@@ -34,6 +37,19 @@ var loadDisplay = function () {
             $('#p1-display').hide();
             console.log('on loadDisplay, snapshot.player1 is false');
         }
+        if (snapshot.val().p2exists === true) {
+            $('#p2-userInput').hide();
+            $('#p2-exit').show();
+            $('.p2choice').show();
+            $('#p2-display').show();
+            console.log('on loadDisplay, snapshot.player2 is true');
+        } else {
+            $('#p2-userInput').show();
+            $('#p2-exit').hide();
+            $('.p2choice').hide();
+            $('#p2-display').hide();
+            console.log('on loadDisplay, snapshot.player2 is false');
+        }
     });
 };
 
@@ -48,7 +64,7 @@ $('#p1-submitButton').click(function (e) {
             p1name: $('#p1-name').val().trim(),
             p1wins: 0,
             p1loss: 0,
-            p1chat: ''
+            p1chat: '',
         }
     });
     $('#p1-userInput').hide();
@@ -57,12 +73,32 @@ $('#p1-submitButton').click(function (e) {
     $('#p1-display').show();
 });
 
+$('#p2-submitButton').click(function (e) {
+    e.preventDefault();
+    database.ref().set({
+        p2exists: true,
+        player2: {
+            p2name: $('#p2-name').val().trim(),
+            p2wins: 0,
+            p2loss: 0,
+            p2chat: '',
+        }
+    });
+    $('#p2-userInput').hide();
+    $('#p2-exit').show();
+    $('.p2choice').show();
+    $('#p2-display').show();
+});
+
+
+// Player exit
 $('#p1-exit').click(function (e) {
     e.preventDefault();
+    $('#p1-name').val('');
     database.ref().set({
         p1exists: false
     });
-    removeRef = database.ref('player1');
+    removeRef = database.ref.child('player1');
     removeRef.remove()
         .then(function () {
             console.log('Player 1 removed.');
@@ -73,11 +109,51 @@ $('#p1-exit').click(function (e) {
         });
 });
 
+$('#p2-exit').click(function (e) {
+    e.preventDefault();
+    $('#p2-name').val('');
+    database.ref().set({
+        p2exists: false
+    });
+    removeRef = database.ref.child('player2');
+    removeRef.remove()
+        .then(function () {
+            console.log('Player 2 removed.');
+            $('#p2-userInput').show();
+            $('#p2-exit').hide();
+            $('.p2choice').hide();
+            $('#p2-display').hide();
+        });
+});
+
+///////// GAME ///////////
 $('.p1choice').click(function () {
     p1choice = $(this).attr('value');
     console.log('p1choice: ' + p1choice);
     p1selected = true;
+    $(".p1choice").prop("disabled", true);
+    if (p1selected === true && p2selected === true) {
+        compare();
+        $('.p1choice').prop('disabled',false);
+    }
 });
+
+$('.p2choice').click(function () {
+    p2choice = $(this).attr('value');
+    console.log('p2choice: ' + p2choice);
+    p2selected = true;
+    $(".p2choice").prop("disabled", true);
+    if (p1selected === true && p2selected === true) {
+        compare();
+        $('.p2choice').prop('disabled',false);
+    };
+});
+
+var compare = function () {
+    // if check, attribute win, run values
+    $('.p1choice').prop('disabled',false);
+    $('.p2choice').prop('disabled',false);
+}
 
 ////////////// PULLS ON DATA CHANGES /////////////////
 
@@ -88,7 +164,12 @@ database.ref().on('value', function (snapshot) {
     if (snapshot.val().p1exists === true) {
         console.log('on value update, snapshot.player1 is true');
         p1name = snapshot.val().player1.p1name; // Updating local var
-        $('#p1-display').text('Player 1: ' + p1name); // Displaying local var
+        $('#p1-display').text(p1name); // Displaying local var
+    };
+    if (snapshot.val().p2exists === true) {
+        console.log('on value update, snapshot.player1 is true');
+        p2name = snapshot.val().player2.p2name; // Updating local var
+        $('#p2-display').text(p2name); // Displaying local var
     };
 }, function (errorObject) {
     console.log('The read failed: ' + errorObject.code);
